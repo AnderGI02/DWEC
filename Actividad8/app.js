3755fdc3b4d274686d5b4c8322b26d3f06feb3f6
AnderGI <ander.gutierrez.ibarguen@zabalburu.org>
2023-10-20 10:45:58 +0200

    Actuividad 8

--- Actividad8/app.js
+++ Actividad8/app.js
@@ -4,48 +4,35 @@ const submitBtn = document.querySelector('button[type="submit"]');
 const tablaUsuariosBody = document.querySelector("table#tablaUsuarios > tbody");
 const tablaVotosBody = document.querySelector("table#tablaVotos > tbody");
 
+//OPCIONES AL PRINCIPIO
+const opciones = [];
+const addOpcionesFromNum = (limitNum = 5) => {
+  for (let i = 1; i <= limitNum; i++) {
+    opciones.push(`<option value="${i}">Opcion ${i}</option>`);
+  }
+};
+const renderOpcionesInSelect = (selectEl) =>
+  opciones.forEach((opt) => (selectEl.innerHTML += opt));
+
+const renderDataInTable = (tableEl, cb) => {
+  tableEl.replaceChildren();
+  cb();
+};
+
+const renderOpciones = () => {
+  addOpcionesFromNum();
+  renderOpcionesInSelect(document.querySelector("select#voto"));
+};
+
+renderOpciones();
+
+//OPCIONES
+
 submitBtn.addEventListener("click", handleFormSubmit);
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-  const userOptionForm = document.querySelector("form");
-  getUserOption(userOptionForm, ({ usuario, voto }) =>
-    addVote({ usuario, voto })
-  );
-}
-
-function renderTablaUsuarios() {
-  tablaUsuariosBody.replaceChildren();
-  const createUserRow = (user) => `<tr>
-    <th scope="col">${user}</th>
-  </tr>`;
-
-  for (const user of users) {
-    tablaUsuariosBody.innerHTML += createUserRow(user);
-  }
-}
-
-function renderTablaVotos() {}
-
-function addVote({ usuario, voto }) {
-  //Mirar si el usuario no esta en el set
-  //Seria la primera vez
-  //Se añade al set y al map el clave de usuario: voto
-  if (!users.has(usuario)) {
-    users.add(usuario);
-    votos.set(usuario, voto);
-  }
-
-  if (users.has(usuario)) {
-    let votosUsu = votos.get(usuario);
-    votos.set(usuario, ++votosUsu);
-  }
-
-  renderTablaUsuarios();
-  renderTablaVotos();
-}
-
 function getUserOption(form, cb) {
+  //COGE LOS ELEMENTOS NECESARIOS DEL FORMULARIO
+  //LY LOS UTILIZA PARA GENERAR UN OBJETO QUE SE LE PASA A LA IMPLEMENTACION DE CALLBACK
   const neededFormElements = [...form.elements].filter((el) =>
     el.getAttribute("id")
   );
@@ -56,4 +43,43 @@ function getUserOption(form, cb) {
     data[key] = value;
   }
   cb(data);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const userOptionForm = document.querySelector("form");
+  //nombre de usuario y la opcion a la que va a votar
+  getUserOption(userOptionForm, ({ usuario, voto }) =>
+    addUserToSetAndVoteToMap({ usuario, voto })
+  );
+}
+
+function addUserToSetAndVoteToMap({ usuario, voto }) {
+  //El usuario solopuede votar una vez por lo que no tiene que estra en el set antes de añadirlo
+  if (!users.has(usuario)) {
+    users.add(usuario);
+    //Mira si la opcion esta en el map
+    if (!votos.has(voto)) {
+      votos.set(voto, 1);
+    } else {
+      let votosOpc = votos.get(voto);
+      votosOpc += 1;
+      votos.set(voto, votosOpc);
+    }
+    renderDataInTable(tablaUsuariosBody, () => {
+      for (const [key, value] of users.entries()) {
+        tablaUsuariosBody.innerHTML += `<tr>
+        <th scope="col">${value}</th>
+      </tr>`;
+      }
+    });
+    renderDataInTable(tablaVotosBody, () => {
+      for (const [key, value] of votos.entries()) {
+        tablaVotosBody.innerHTML += `<tr>
+        <th scope="col">Opción ${key}</th>
+        <th scope="col" >${value}</th>
+      </tr>`;
+      }
+    });
+  }
 }
